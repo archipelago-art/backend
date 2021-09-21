2e9b11fe22d92c69da01d03a7cbf9927e95ff407
William Chargin <wchargin@gmail.com>
2021-09-21 14:56:15 -0700

    Include ID in Art Blocks project data struct

--- src/fetchArtblocksProject.js
+++ src/fetchArtblocksProject.js
@@ -3,19 +3,26 @@ const htmlParser = require("node-html-parser");
 
 const PROJECT_URL_BASE = "https://api.artblocks.io/project";
 
+function normalizeProjectId(projectId) {
+  const result = Number.parseInt(projectId, 10);
+  if (Number.isNaN(result)) throw new Error("Invalid project ID: " + projectId);
+  return result;
+}
+
 async function fetchProjectHtml(projectId) {
-  const url = `${PROJECT_URL_BASE}/${projectId}`;
+  const url = `${PROJECT_URL_BASE}/${normalizeProjectId(projectId)}`;
   const res = await fetch(url);
   if (!res.ok)
     throw new Error(`fetching ${url}: ${res.status} ${res.statusText}`);
   return await res.text();
 }
 
-function parseProjectData(html) {
+function parseProjectData(projectId, html) {
   const body = htmlParser.parse(html);
   return {
+    projectId: normalizeProjectId(projectId),
     name: findByInnerText(body, "h1", /^Name: (.*)$/)[1],
-    size: Number.parseInt(
+    maxInvocations: Number.parseInt(
       findByInnerText(body, "p", /^Maximum Invocations: ([0-9]+)$/)[1],
       10
     ),
@@ -35,7 +42,7 @@ function findByInnerText(root, selector, re) {
 }
 
 async function fetchProjectData(projectId) {
-  return parseProjectData(await fetchProjectHtml(projectId));
+  return parseProjectData(projectId, await fetchProjectHtml(projectId));
 }
 
 module.exports = {
