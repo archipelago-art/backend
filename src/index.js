bf92daebab226e21fcf843c9c72ee14652eb9f95
William Chargin <wchargin@gmail.com>
2021-09-24 11:58:48 -0700

    main: use separate DB clients to add multiple tokens
    As written, this could have had DB writes stomping on each other, since
    it used only one client with multiple concurrent workers. (Sure would be
    nice to have some kind of type system enforcing exclusive references to
    exclusive resources.)

--- src/index.js
+++ src/index.js
@@ -2,28 +2,28 @@ const pg = require("pg");
 
 const artblocks = require("./db/artblocks");
 const migrations = require("./db/migrations");
+const { acqrel } = require("./db/util");
 const { fetchProjectData } = require("./scrape/fetchArtblocksProject");
 const { fetchTokenData } = require("./scrape/fetchArtblocksToken");
 
-async function withClient(callback) {
-  const client = new pg.Client();
-  await client.connect();
+async function withDb(callback) {
+  const pool = new pg.Pool();
   try {
-    return await callback(client);
+    return await acqrel(pool, (client) => callback({ pool, client }));
   } finally {
-    await client.end();
+    await pool.end();
   }
 }
 
 async function init() {
-  await withClient(async (client) => {
+  await withDb(async ({ client }) => {
     await migrations.applyAll({ client, verbose: true });
   });
 }
 
 async function addProject(args) {
   const [projectId] = args;
-  await withClient(async (client) => {
+  await withDb(async ({ client }) => {
     const project = await fetchProjectData(projectId);
     await artblocks.addProject({ client, project });
   });
@@ -31,14 +31,14 @@ async function addProject(args) {
 
 async function getProject(args) {
   const [projectId] = args;
-  await withClient(async (client) => {
+  await withDb(async ({ client }) => {
     console.log(await artblocks.getProject({ client, projectId }));
   });
 }
 
 async function addToken(args) {
   const [tokenId] = args;
-  await withClient(async (client) => {
+  await withDb(async ({ client }) => {
     const token = await fetchTokenData(tokenId);
     await artblocks.addToken({ client, tokenId, rawTokenData: token.raw });
   });
@@ -46,7 +46,7 @@ async function addToken(args) {
 
 async function getFeaturesOfToken(args) {
   const [tokenId] = args;
-  await withClient(async (client) => {
+  await withDb(async ({ client }) => {
     const features = await artblocks.getTokenFeatures({ client, tokenId });
     for (const feature of features) console.log(feature);
   });
@@ -54,7 +54,7 @@ async function getFeaturesOfToken(args) {
 
 async function getFeaturesOfProject(args) {
   const projectId = Number(args[0]);
-  await withClient(async (client) => {
+  await withDb(async ({ client }) => {
     const features = await artblocks.getProjectFeatures({
       client,
       projectId,
@@ -66,7 +66,7 @@ async function getFeaturesOfProject(args) {
 async function getTokensWithFeature(args) {
   const projectId = Number(args[0]);
   const featureName = args[1];
-  await withClient(async (client) => {
+  await withDb(async ({ client }) => {
     const tokenIds = await artblocks.getTokensWithFeature({
       client,
       projectId,
@@ -78,8 +78,10 @@ async function getTokensWithFeature(args) {
 
 async function addProjectTokens(args) {
   const [projectId] = args;
-  await withClient(async (client) => {
-    const ids = await artblocks.getUnfetchedTokenIds({ client, projectId });
+  await withDb(async ({ pool }) => {
+    const ids = await acqrel(pool, (client) =>
+      artblocks.getUnfetchedTokenIds({ client, projectId })
+    );
     console.log(`got ${ids.length} missing IDs`);
     const chunks = [];
     async function worker() {
@@ -88,11 +90,13 @@ async function addProjectTokens(args) {
         if (tokenId == null) return;
         try {
           const token = await fetchTokenData(tokenId);
-          await artblocks.addToken({
-            client,
-            tokenId,
-            rawTokenData: token.raw,
-          });
+          await acqrel(pool, (client) =>
+            artblocks.addToken({
+              client,
+              tokenId,
+              rawTokenData: token.raw,
+            })
+          );
           console.log("added token " + tokenId);
         } catch (e) {
           console.log("failed to add token " + tokenId);
