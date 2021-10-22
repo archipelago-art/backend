bb08b47e84f1330bd57e6214953bb506595e4d47
Dandelion Mané <decentralion@dandelion.io>
2021-10-22 12:11:23 -0700

    Add testing for tokenFeatures

--- src/api/index.test.js
+++ src/api/index.test.js
@@ -53,4 +53,44 @@ describe("api", () => {
       ]);
     })
   );
+
+  it(
+    "provides tokenFeatures",
+    withTestDb(async ({ client }) => {
+      const archetype = parseProjectData(
+        snapshots.ARCHETYPE,
+        await sc.project(snapshots.ARCHETYPE)
+      );
+      const collection = api.artblocksProjectIdToCollectionName(
+        archetype.projectId
+      );
+      for (const tokenId of snapshots.TOKENS) {
+        const rawTokenData = await sc.token(tokenId);
+        await artblocks.addToken({ client, tokenId, rawTokenData });
+      }
+      const res = await api.tokenFeatures({ client, collection });
+      const expected = {
+        featureNames: [
+          "Coloring strategy: Random",
+          "Framed: Yep",
+          "Layout: Order",
+          "Palette: Paddle",
+          "Scene: Flat",
+          "Shading: Noon",
+          "Coloring strategy: Group",
+          "Coloring strategy: Single",
+          "Layout: Chaos",
+          "Scene: Cube",
+          "Shading: Bright Morning",
+        ],
+        tokens: {
+          23000036: [0, 1, 2, 3, 4, 5],
+          23000045: [6, 1, 2, 3, 4, 5],
+          23000250: [7, 1, 8, 3, 9, 10],
+          23000467: [6, 1, 2, 3, 4, 5],
+        },
+      };
+      expect(res).toEqual(expected);
+    })
+  );
 });
