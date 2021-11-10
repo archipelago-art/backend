ec3da54e34030eba51eda816fde9ed01992adc1f
William Chargin <wchargin@gmail.com>
2021-11-10 09:00:26 -0800

    ws: allow requesting latest tokens in a collection

--- src/ws/index.js
+++ src/ws/index.js
@@ -1,5 +1,6 @@
 const util = require("util");
 
+const api = require("../api");
 const artblocks = require("../db/artblocks");
 const { acqrel } = require("../db/util");
 
@@ -13,10 +14,25 @@ type PongMessage = {
   nonce: string,
 };
 
+// Sent from server to client when new tokens are minted.
 type NewTokensMessage = {
   type: "NEW_TOKENS",
   tokens: TokenData[],
 };
+
+// Sent from client to server to request tokens for a collection.
+type GetLatestTokensMessage = {
+  type: "GET_LATEST_TOKENS",
+  collection: string,
+  lastTokenId: number | null,  // fetch only token IDs higher than this one
+}
+
+// Sent from server to client to indicate a bad incoming message.
+type ErrorMessage = {
+  type: "ERROR",
+  httpStatus: number,
+  message: string,
+}
 
 type TokenData = {
   tokenId: number,
@@ -78,11 +94,12 @@ async function attach(server, pool) {
         }
         switch (payload.type) {
           case "PING":
-            send(
-              client,
-              JSON.stringify({ type: "PONG", nonce: payload.nonce })
-            );
+            handlePing(client, pool, payload);
             break;
+          case "GET_LATEST_TOKENS":
+            handleGetLatestTokens(client, pool, payload);
+            break;
+
           default:
             console.warn("ignoring unknown message type: %s", payload.type);
         }
@@ -95,12 +112,61 @@ async function attach(server, pool) {
   }
 }
 
+function handlePing(client, pool, payload) {
+  sendJson(ws, { type: "PONG", nonce: payload.nonce });
+}
+
+async function handleGetLatestTokens(client, pool, payload) {
+  const { collection, lastTokenId } = payload;
+  if (typeof collection !== "string") {
+    sendJson(client, {
+      type: "ERROR",
+      httpStatus: 400,
+      message: "collection should be a string",
+    });
+    return;
+  }
+  const projectId = api.collectionNameToArtblocksProjectId(collection);
+  if (projectId == null) {
+    sendJson(client, {
+      type: "ERROR",
+      httpStatus: 400,
+      message: "invalid collection ID",
+    });
+    return;
+  }
+  if (
+    lastTokenId !== null &&
+    (!Number.isInteger(lastTokenId) || !(lastTokenId >= 0))
+  ) {
+    sendJson(client, {
+      type: "ERROR",
+      httpStatus: 400,
+      message: "lastTokenId should be a non-negative integer or null",
+    });
+    return;
+  }
+  const minTokenId = lastTokenId == null ? 0 : lastTokenId + 1;
+  const tokens = await acqrel(pool, async (client) => {
+    return artblocks.getTokenFeaturesAndTraits({
+      client,
+      projectId,
+      minTokenId,
+    });
+  });
+
+  sendJson(client, { type: "NEW_TOKENS", tokens });
+}
+
 function send(client, msg) {
   util
     .promisify(client.send.bind(client))(msg)
     .catch((e) => {
       console.error("failed to send message (%s) to client:", msg, e);
     });
+}
+function sendJson(client, json) {
+  send(client, JSON.stringify(json));
 }
 
 module.exports = attach;
