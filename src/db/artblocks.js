d59d180423f9390ef98c72a5e2e2ea361764856d
Dandelion Mané <decentralion@dandelion.io>
2021-12-21 15:18:02 -0800

    clarify comment re: legacy artblocks contract ids

--- src/db/artblocks.js
+++ src/db/artblocks.js
@@ -10,7 +10,7 @@ const PROJECT_STRIDE = 1e6;
 const CONTRACT_ARTBLOCKS_LEGACY = "0x059EDD72Cd353dF5106D2B9cC5ab83a52287aC3a";
 const CONTRACT_ARTBLOCKS_STANDARD =
   "0xa7d8d9ef8D8Ce8992Df33D8b8CF4Aebabd5bD270";
-// Projects below this threshold are legacy, above are standard.
+// Projects below this threshold are legacy, at or above are standard.
 const ARTBLOCKS_CONTRACT_THRESHOLD = 3;
 
 function artblocksContractAddress(projectId) {
