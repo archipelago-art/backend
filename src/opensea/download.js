d852c04ff772400d586bf6675f99f56870c277b5
Dandelion Mané <decentralion@dandelion.io>
2022-01-18 18:13:35 -0800

    opensea: add fast sync for recently updated projects

--- src/opensea/download.js
+++ src/opensea/download.js
@@ -6,10 +6,11 @@ const {
 } = require("../db/opensea/progress");
 const log = require("../util/log")(__filename);
 const { initializeArtblocksProgress } = require("./artblocksProgress");
-const { fetchEventsByTypes } = require("./fetch");
+const { fetchEventsByTypes, fetchEvents } = require("./fetch");
 
 const ONE_MONTH = 1000 * 60 * 60 * 24 * 30;
 const LATE_EVENT_SAFETY_MARGIN = 1000 * 60 * 3;
+const FAST_SYNC_THRESHOLD = 1000 * 60 * 60; // one hour
 const BEGINNING_OF_HISTORY = new Date("2020-11-27");
 
 /**
@@ -94,18 +95,48 @@ async function downloadCollection({
   while (!(await downloadWindow(args)));
 }
 
+async function syncProject({ client, slug, projectId, apiKey }) {
+  const since = await getLastUpdated({ client, slug, projectId });
+  const until = new Date();
+  const events = await fetchEvents({
+    source: { slug },
+    since,
+    until,
+    apiKey,
+  });
+  const relevantEvents = events.filter(
+    (x) =>
+      x.event_type === "created" ||
+      x.event_type === "successful" ||
+      x.event_type === "cancelled"
+  );
+  const strippedEvents = relevantEvents
+    .filter((x) => x.asset != null)
+    .map(stripEvent);
+  await addRawEvents({ client, events: strippedEvents });
+  log.info`${slug}: added ${strippedEvents.length} events during fast sync`;
+  const updated = new Date(Date.now() - LATE_EVENT_SAFETY_MARGIN);
+  await setLastUpdated({ client, slug, until, projectId });
+}
+
 async function downloadAllCollections({ client, apiKey, windowDurationMs }) {
   await initializeArtblocksProgress({ client, apiKey });
   const progress = await getProgress({ client });
-  for (const { slug, projectId } of progress) {
-    log.info`=== ingesting opensea events for ${slug} ===`;
-    await downloadCollection({
-      client,
-      slug,
-      projectId,
-      apiKey,
-      windowDurationMs,
-    });
+  for (const { slug, projectId, lastUpdated } of progress) {
+    const timeSinceLastUpdateMs = Date.now() - +lastUpdated;
+    if (timeSinceLastUpdateMs < FAST_SYNC_THRESHOLD) {
+      log.info`=== fast sync for ${slug} ===`;
+      await syncProject({ client, slug, projectId, apiKey });
+    } else {
+      log.info`=== downloading events for ${slug} ===`;
+      await downloadCollection({
+        client,
+        slug,
+        projectId,
+        apiKey,
+        windowDurationMs,
+      });
+    }
   }
 }
 
