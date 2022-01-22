bca21f3ef9f087e5e266f600ced13905a0a666f1
William Chargin <wchargin@gmail.com>
2022-01-22 11:41:49 -0800

    updateSuspiciousTokens: detect suspicious lack of traits
    If a token has no traits, but other tokens in its project do have
    traits, that's suspicious. The Art Blocks API may have been incorrectly
    reporting that it had no traits.

--- src/cli/updateSuspiciousTokens.js
+++ src/cli/updateSuspiciousTokens.js
@@ -3,16 +3,48 @@ const { acqrel, withPool } = require("../db/util");
 const { fetchTokenData } = require("../scrape/fetchArtblocksToken");
 const log = require("../util/log")(__filename);
 
-const NETWORK_CONCURRENCY = 64;
+const NETWORK_CONCURRENCY = 32;
 
-async function updateSuspiciousTokens() {
+async function updateSuspiciousTokens(args, self) {
+  let checkSusTraits = true;
+  let checkSusTraitless = true;
+  for (const arg of args) {
+    switch (arg) {
+      case "--no-sus-traits":
+        checkSusTraits = false;
+        break;
+      case "--no-sus-traitless":
+        checkSusTraitless = false;
+        break;
+      default:
+        throw new Error(
+          `usage: ${self} [--no-sus-traits] [--no-sus-traitless]`
+        );
+    }
+  }
+
   await withPool(async (pool) => {
+    const [tokensFromSuspiciousTraits, suspiciousTraitlessTokens] =
+      await Promise.all([
+        !checkSusTraits
+          ? []
+          : acqrel(pool, (client) =>
+              artblocks.findSuspiciousTraits({ client })
+            ),
+        !checkSusTraitless
+          ? []
+          : acqrel(pool, (client) =>
+              artblocks.findSuspiciousTraitlessTokens({ client })
+            ),
+      ]);
+    const tokenIds = Array.from(
+      new Set([...tokensFromSuspiciousTraits, ...suspiciousTraitlessTokens])
+    );
     const artblocksTokenIds = await acqrel(pool, async (client) => {
-      const tokenIds = await artblocks.findSuspiciousTraits({ client });
       const res = await artblocks.getArtblocksTokenIds({ client, tokenIds });
       return res.sort((a, b) => a.artblocksTokenId - b.artblocksTokenId);
     });
-    log.info`got ${artblocksTokenIds.length} suspicious token IDs`;
+    log.info`got ${artblocksTokenIds.length} suspicious token IDs (${tokensFromSuspiciousTraits.length} from sus traits, ${suspiciousTraitlessTokens.length} traitless)`;
     async function worker() {
       await acqrel(pool, async (client) => {
         while (true) {
@@ -20,14 +52,18 @@ async function updateSuspiciousTokens() {
           if (item == null) return;
           const { tokenId, artblocksTokenId } = item;
           log.debug`fetching token ${artblocksTokenId} (token ID ${tokenId})`;
-          const token = await fetchTokenData(artblocksTokenId);
-          const rawTokenData = token.raw;
-          if (rawTokenData == null) {
-            log.info`token ${token} not found; skipping`;
-            return;
+          try {
+            const token = await fetchTokenData(artblocksTokenId);
+            const rawTokenData = token.raw;
+            if (rawTokenData == null) {
+              log.info`token ${token} not found; skipping`;
+              return;
+            }
+            await artblocks.updateTokenData({ client, tokenId, rawTokenData });
+            log.info`updated token data for token ${artblocksTokenId}`;
+          } catch (e) {
+            log.warn`failed to add token ${artblocksTokenId}: ${e}`;
           }
-          await artblocks.updateTokenData({ client, tokenId, rawTokenData });
-          log.info`updated token data for token ${artblocksTokenId}`;
         }
       });
     }
