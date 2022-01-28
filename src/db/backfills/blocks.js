68497458b2656ad526bbb81df7338fc15642fe7e
William Chargin <wchargin@gmail.com>
2022-01-27 23:38:07 -0800

    backfills/blocks: only query missing hashes once
    This was getting quadratically slower, and then suddenly became much
    slower (query plan change?).

--- src/db/backfills/blocks.js
+++ src/db/backfills/blocks.js
@@ -2,22 +2,20 @@ const { ingestBlocks } = require("../../eth/tokenTransfers");
 const log = require("../../util/log")(__filename);
 const { acqrel, bufToHex } = require("../util");
 
-const RPC_BATCH_SIZE = 256;
+const RPC_BATCH_SIZE = 128;
 const QUERY_BATCH_SIZE = RPC_BATCH_SIZE * 4;
 
 async function backfillBlocks({ pool, verbose }) {
   let total = 0;
-  while (true) {
-    const res = await pool.query(
-      `
-      SELECT DISTINCT block_hash AS "blockHash" FROM erc_721_transfers
-      WHERE block_hash NOT IN (SELECT block_hash FROM eth_blocks)
-      LIMIT $1
-      `,
-      [QUERY_BATCH_SIZE]
-    );
-    if (res.rowCount === 0) break;
-    const blockHashes = res.rows.map((r) => bufToHex(r.blockHash));
+  const res = await pool.query(
+    `
+    SELECT DISTINCT block_hash AS "blockHash" FROM erc_721_transfers
+    WHERE block_hash NOT IN (SELECT block_hash FROM eth_blocks)
+    `
+  );
+  const allBlockHashes = res.rows.map((r) => bufToHex(r.blockHash));
+  for (let i = 0; i < allBlockHashes.length; i += QUERY_BATCH_SIZE) {
+    const blockHashes = allBlockHashes.slice(i, i + QUERY_BATCH_SIZE);
     total += await ingestBlocks({
       pool,
       blockHashes,
