18846afc1e2354954a58d9f5180223bbc1a9fa8a
William Chargin <wchargin@gmail.com>
2022-02-01 21:32:13 -0800

    dumpTraitOrdering: make output more diffable and include slugs

--- src/cli/dumpTraitOrdering.js
+++ src/cli/dumpTraitOrdering.js
@@ -1,6 +1,7 @@
 const artblocks = require("../db/artblocks");
 const { withClient } = require("../db/util");
 const log = require("../util/log")(__filename);
+const slugify = require("../util/slugify");
 const sortAsciinumeric = require("../util/sortAsciinumeric");
 
 async function dumpTraitOrdering() {
@@ -8,8 +9,21 @@ async function dumpTraitOrdering() {
     return artblocks.getAllFeaturesAndTraitsOnly({ client });
   });
   for (const row of res) {
-    row.traitValues = sortAsciinumeric(row.traitValues, (t) => String(t));
-    console.log(JSON.stringify(row));
+    console.log(
+      "project %s, feature %s, feature slug %s",
+      JSON.stringify(row.projectSlug),
+      JSON.stringify(row.featureName),
+      JSON.stringify(slugify(row.featureName))
+    );
+    const traits = sortAsciinumeric(row.traitValues, (t) => String(t));
+    for (const trait of traits) {
+      console.log(
+        "trait %s, trait slug %s",
+        JSON.stringify(trait),
+        JSON.stringify(slugify(String(trait)))
+      );
+    }
+    console.log();
   }
 }
 
