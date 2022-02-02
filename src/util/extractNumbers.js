3acc62d3999f9d4bb5728364ee6104a6ca2c9a6e
William Chargin <wchargin@gmail.com>
2022-02-02 10:57:24 -0800

    extractNumbers: reimplement without lookbehind
    This module is used via `smartSort` on the frontend, and Safari does not
    support lookbehind in regular expressions, hence this monstrosity.
    
    The output of `dump-trait-ordering` on prod is unchanged before and
    after this patch, amazingly.

--- src/util/extractNumbers.js
+++ src/util/extractNumbers.js
@@ -1,31 +1,4 @@
-function concat(strs) {
-  return strs.join("");
-}
-// (See docs on `extractNumbers`.)
-//
-const RE_DELIMITER = new RegExp(
-  concat([
-    // Surround delimiter in groups so that `String.prototype.split` retains
-    // the delimiters.
-    "(",
-    // Numbers must be either...
-    concat([
-      "(?:",
-      // ... (a) not preceded by a word character...
-      "(?<!\\w)",
-      // ...or (b) preceded by an "x" (as in "3x3 grid"), unless that "x" is
-      // part of a normal word like "helix" or the start of a "0x..." literal.
-      "|(?<=(?<![A-Za-z_]|(?<!\\d)0)x)",
-      ")",
-    ]),
-    // A number has an optional sign, plus one or more integral-part digits,
-    // followed optionally by a decimal point and one or more fractional-part
-    // digits.
-    "(?:[+-]?[0-9]+(?:\\.[0-9]+)?)",
-    ")",
-  ]),
-  "g"
-);
+const RE_UNSIGNED_DECIMAL = /[0-9]+(?:\.[0-9]+)?/;
 
 /**
  * Splits a string into pieces that, when concatenated, form the original
@@ -44,7 +17,62 @@ const RE_DELIMITER = new RegExp(
  *    - "Limited-time x2 xp event" maps to ["Limited-time x", "2", " xp event"]
  */
 function extractNumbers(s /*: string */) /*: string[] */ {
-  return s.split(RE_DELIMITER).filter(Boolean);
+  const result = [];
+  let i = 0;
+  while (i < s.length) {
+    const num = findNextNumber(s, i);
+    if (num == null) {
+      result.push(s.slice(i));
+      break;
+    }
+    result.push(s.slice(i, num.index));
+    result.push(s.slice(num.index, num.index + num.length));
+    i = num.index + num.length;
+  }
+  return result.filter(Boolean);
+}
+
+// Finds the next possibly signed decimal that's either (a) not preceded by a
+// word character, or (b) preceded by an "x" (as in "3x3 grid"), unless that
+// "x" is part of a normal word like "helix" or the start of a "0x..." literal.
+function findNextNumber(s, i) {
+  while (i < s.length) {
+    const match = s.slice(i).match(RE_UNSIGNED_DECIMAL);
+    if (match == null) break;
+    const matchStart = i + match.index;
+    if (s[matchStart - 1] === "+" || s[matchStart - 1] === "-") {
+      // Prefer interpreting as signed.
+      if (mayBeUnsignedDecimal(s, matchStart - 1)) {
+        return { index: matchStart - 1, length: match[0].length + 1 };
+      }
+    }
+    if (mayBeUnsignedDecimal(s, matchStart)) {
+      return { index: matchStart, length: match[0].length };
+    }
+    i += matchStart + match[0].length;
+  }
+  return null;
+}
+
+function mayBeUnsignedDecimal(s, i) {
+  // Numbers may always appear at start of string.
+  if (i === 0) return true;
+  // Numbers may be preceded by non-word characters.
+  if (!s[i - 1].match(/\w/)) return true;
+  // Numbers preceded by a word character other than an "x" are considered part
+  // of the preceding word.
+  if (s[i - 1] !== "x") return false;
+  // An "x" at the start of the string (as in "x2 multiplier") is okay.
+  if (i === 1) return true;
+  // An "x" preceded by a non-digit word character (as in "helix") doesn't
+  // count as a separator.
+  if (s[i - 2].match(/[A-Za-z_]/)) return false;
+  // Otherwise, an "x" is a separator unless it's preceded by a lone "0", which
+  // we interpret as a "0x1234" hex literal. A longer number ending in zero, as
+  // in "10x10 grid", is fine.
+  if (s[i - 2] !== "0") return true;
+  if (i === 2) return false; // "0x" at start of string
+  return s[i - 2].match(/[0-9]/);
 }
 
 module.exports = extractNumbers;
