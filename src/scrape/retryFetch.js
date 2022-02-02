ac7d1ee055a93b9e805d1e045c96cf2948273dd4
Dandelion Mané <decentralion@dandelion.io>
2022-02-02 05:59:39 -0800

    retryFetch: wait on 429, rather than failing
    Defaults to a 30 second wait because the OpenSea API is generally asking
    us to wait for <15 seconds, so this is a comfortable margin of error.

--- src/scrape/retryFetch.js
+++ src/scrape/retryFetch.js
@@ -4,6 +4,12 @@ const retry = require("../util/retry");
 
 const FETCH_TIMEOUT_NEEDLE = "network timeout at:";
 
+// Chose 30 second delay on 429 based on looking at OpenSea's suggested retry times, which
+// were always <15 seconds.
+// We may want to move this to a tuning parameter if we find other services need different
+// wait policies.
+const WAIT_DELAY_MS_FOR_429 = 20000;
+
 async function fetchWithRetries(url, fetchOptions = { timeout: 5000 }) {
   const result = await retry(async () => {
     let res;
@@ -15,6 +21,10 @@ async function fetchWithRetries(url, fetchOptions = { timeout: 5000 }) {
       const result = { res, text };
       if (res.ok) {
         return { type: "DONE", value: result };
+      } else if (res.status === 429) {
+        const until = new Date(Date.now() + WAIT_DELAY_MS_FOR_429);
+        console.warn("waiting due to 429 at %s", url);
+        return { type: "WAIT", until, err: result };
       } else if (res.status >= 500) {
         console.warn(
           "retrying due to server error: %s %s at %s",
