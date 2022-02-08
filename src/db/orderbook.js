433dcffb1f080dd3646789c2a4a097dc29350cd6
Dandelion Mané <decentralion@dandelion.io>
2022-02-08 13:55:15 -0700

    orderbook: implement `addBid`

---
+++ src/db/orderbook.js
@@ -0,0 +1,128 @@
+const { ObjectType, newId } = require("./id");
+const { hexToBuf } = require("./util");
+
+/**
+ * type Scope = ProjectScope | TokenScope | TraitScope | CnfScope;
+ * type ProjectScope = {type: "PROJECT", projectId}
+ * type TokenScope = {type: "TOKEN", tokenId}
+ * type TraitScope = {type: "TRAIT", traitId}
+ * type CnfScope = empty; // not yet implemented
+ */
+async function addBid({
+  client,
+  scope /*: Scope */,
+  price /*: ethers.BigNumber */,
+  deadline /*: Date */,
+  bidder /*: address */,
+  nonce /*: uint256 */,
+  agreement /*: bytes: ABI-encoded [OrderAgreement] */,
+  message /*: bytes: ABI-encoded [Bid] */,
+  signature /*: bytes65 */,
+}) {
+  await client.query("BEGIN");
+  let projectId, scopeId;
+  switch (scope.type) {
+    case "PROJECT":
+      await checkProjectExists(client, scope.projectId);
+      projectId = scope.projectId;
+      scopeId = projectId;
+      break;
+    case "TOKEN":
+      projectId = await projectForTokenId(client, scope.tokenId);
+      scopeId = scope.tokenId;
+      break;
+    case "TRAIT":
+      projectId = await projectForTraitId(client, scope.traitId);
+      scopeId = scope.traitId;
+      break;
+    case "CNF":
+      throw new Error("CNF scopes not implemented");
+      break;
+    default:
+      throw new Error(`Unrecognized scope type: ${scope.type}`);
+  }
+  const bidId = newId(ObjectType.BID);
+  await client.query(
+    `
+    INSERT INTO bids (
+      bid_id,
+      project_id,
+      scope,
+      active,
+      price,
+      deadline,
+      create_time,
+      bidder,
+      nonce,
+      agreement,
+      message,
+      signature
+    ) VALUES (
+      $1::bidid,
+      $2::projectid,
+      $3::bidscope,
+      true,
+      $4::uint256,
+      $5::timestamptz,
+      now(),
+      $6::address,
+      $7::uint256,
+      $8::bytea,
+      $9::bytea,
+      $10::signature
+    )
+    `,
+    [
+      bidId,
+      projectId,
+      scopeId,
+      String(price),
+      deadline,
+      hexToBuf(bidder),
+      String(nonce),
+      hexToBuf(agreement),
+      hexToBuf(message),
+      hexToBuf(signature),
+    ]
+  );
+  await client.query("COMMIT");
+  return bidId;
+}
+
+async function checkProjectExists(client, projectId) {
+  const res = await client.query(
+    `
+    SELECT 1 FROM projects WHERE project_id = $1
+    `,
+    [projectId]
+  );
+  if (res.rows.length !== 1) throw new Error(`no such project: ${projectId}`);
+}
+
+async function projectForTokenId(client, tokenId) {
+  const res = await client.query(
+    `
+    SELECT project_id AS "id" FROM tokens WHERE token_id = $1
+    `,
+    [tokenId]
+  );
+  if (res.rows.length !== 1) throw new Error(`no such token: ${tokenId}`);
+  return res.rows[0].id;
+}
+
+async function projectForTraitId(client, traitId) {
+  const res = await client.query(
+    `
+    SELECT project_id AS "id"
+    FROM traits JOIN features USING (feature_id)
+    WHERE trait_id = $1
+    `,
+    [traitId]
+  );
+  if (res.rows.length !== 1) throw new Error(`no such trait: ${traitId}`);
+  return res.rows[0].id;
+}
+
+module.exports = {
+  addBid,
+};
---
+++ src/db/orderbook.test.js
@@ -0,0 +1,181 @@
+const ethers = require("ethers");
+
+const { parseProjectData } = require("../scrape/fetchArtblocksProject");
+const snapshots = require("../scrape/snapshots");
+const artblocks = require("./artblocks");
+const { addBid } = require("./orderbook");
+const { testDbProvider } = require("./testUtil");
+
+describe("db/orderbook", () => {
+  const withTestDb = testDbProvider();
+  const sc = new snapshots.SnapshotCache();
+
+  async function addProjects(client, projectIds) {
+    const projects = await Promise.all(
+      projectIds.map(async (id) => parseProjectData(id, await sc.project(id)))
+    );
+    const result = [];
+    for (const project of projects) {
+      const id = await artblocks.addProject({ client, project });
+      result.push(id);
+    }
+    return result;
+  }
+
+  async function addTokens(client, artblocksTokenIds) {
+    const tokens = await Promise.all(
+      artblocksTokenIds.map(async (artblocksTokenId) => ({
+        artblocksTokenId,
+        rawTokenData: await sc.token(artblocksTokenId),
+      }))
+    );
+    const result = [];
+    for (const { artblocksTokenId, rawTokenData } of tokens) {
+      const id = await artblocks.addToken({
+        client,
+        artblocksTokenId,
+        rawTokenData,
+      });
+      result.push(id);
+    }
+    return result;
+  }
+
+  async function getBids(client) {
+    const res = await client.query(`
+      SELECT bid_id AS "bidId", scope AS "scope" FROM bids
+    `);
+    return res.rows;
+  }
+  async function isBidActive(client, bidId) {
+    const res = await client.query(
+      `
+      SELECT active FROM bids WHERE bid_id = $1::bidid
+      `,
+      [bidId]
+    );
+    if (res.rowCount !== 1) throw new Error(`no such bid: ${bidId}`);
+    return res.rows[0].active;
+  }
+
+  describe("addBid", () => {
+    it(
+      "adds a bid with project scope",
+      withTestDb(async ({ client }) => {
+        const [archetype] = await addProjects(client, [snapshots.ARCHETYPE]);
+        const bidId = await addBid({
+          client,
+          scope: { type: "PROJECT", projectId: archetype },
+          price: ethers.BigNumber.from("100"),
+          deadline: new Date("2099-01-01"),
+          bidder: ethers.constants.AddressZero,
+          nonce: ethers.BigNumber.from("0xabcd"),
+          agreement: "0x",
+          message: "0x",
+          signature: "0x" + "fe".repeat(65),
+        });
+        expect(await getBids(client)).toEqual([{ bidId, scope: archetype }]);
+      })
+    );
+
+    it(
+      "adds a bid with token scope",
+      withTestDb(async ({ client }) => {
+        const [archetype] = await addProjects(client, [snapshots.ARCHETYPE]);
+        const [theCube] = await addTokens(client, [snapshots.THE_CUBE]);
+        const bidId = await addBid({
+          client,
+          scope: { type: "TOKEN", tokenId: theCube },
+          price: ethers.BigNumber.from("100"),
+          deadline: new Date("2099-01-01"),
+          bidder: ethers.constants.AddressZero,
+          nonce: ethers.BigNumber.from("0xabcd"),
+          agreement: "0x",
+          message: "0x",
+          signature: "0x" + "fe".repeat(65),
+        });
+        expect(await getBids(client)).toEqual([{ bidId, scope: theCube }]);
+      })
+    );
+
+    it(
+      "adds a bid with trait scope",
+      withTestDb(async ({ client }) => {
+        const [archetype] = await addProjects(client, [snapshots.ARCHETYPE]);
+        const [theCube] = await addTokens(client, [snapshots.THE_CUBE]);
+        const traitData = await artblocks.getTokenFeaturesAndTraits({
+          client,
+          tokenId: theCube,
+        });
+        expect(traitData).toEqual([
+          expect.objectContaining({
+            tokenId: theCube,
+            traits: expect.arrayContaining([
+              expect.objectContaining({
+                name: "Palette",
+                value: "Paddle",
+                traitId: expect.any(String),
+              }),
+            ]),
+          }),
+        ]);
+        const { traitId } = traitData[0].traits.find(
+          (t) => t.name === "Palette" && t.value === "Paddle"
+        );
+        const bidId = await addBid({
+          client,
+          scope: { type: "TRAIT", traitId },
+          price: ethers.BigNumber.from("100"),
+          deadline: new Date("2099-01-01"),
+          bidder: ethers.constants.AddressZero,
+          nonce: ethers.BigNumber.from("0xabcd"),
+          agreement: "0x",
+          message: "0x",
+          signature: "0x" + "fe".repeat(65),
+        });
+        expect(await getBids(client)).toEqual([{ bidId, scope: traitId }]);
+      })
+    );
+
+    it(
+      "errors on CNF scope",
+      withTestDb(async ({ client }) => {
+        await expect(() =>
+          addBid({
+            client,
+            scope: { type: "CNF" },
+            price: ethers.BigNumber.from("100"),
+            deadline: new Date("2099-01-01"),
+            bidder: ethers.constants.AddressZero,
+            nonce: ethers.BigNumber.from("0xabcd"),
+            agreement: "0x",
+            message: "0x",
+            signature: "0x" + "fe".repeat(65),
+          })
+        ).rejects.toThrow("CNF scopes not implemented");
+      })
+    );
+
+    it(
+      "always sets bids to active",
+      withTestDb(async ({ client }) => {
+        const [archetype] = await addProjects(client, [snapshots.ARCHETYPE]);
+        const deadline = new Date("2000-01-01"); // expired!
+        const bidId = await addBid({
+          client,
+          scope: { type: "PROJECT", projectId: archetype },
+          price: ethers.BigNumber.from("100"),
+          deadline,
+          bidder: ethers.constants.AddressZero,
+          nonce: ethers.BigNumber.from("0xabcd"),
+          agreement: "0x",
+          message: "0x",
+          signature: "0x" + "fe".repeat(65),
+        });
+        const bids = await getBids(client);
+        const active = await isBidActive(client, bids[0].bidId);
+        expect(active).toBe(true); // for now...
+      })
+    );
+  });
+});
