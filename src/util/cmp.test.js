2989b98bdf4f2d6c6a86919a7962d71ecb7e9fff
William Chargin <wchargin@gmail.com>
2022-02-10 14:57:18 -0700

    cmp: add tests

---
+++ src/util/cmp.test.js
@@ -0,0 +1,73 @@
+const Cmp = require("./cmp");
+
+describe("util/cmp", () => {
+  describe("natural", () => {
+    it("compares strings naturally", () => {
+      const actual = ["bob", "alice", "cheryl"].sort(Cmp.natural);
+      expect(actual).toEqual(["alice", "bob", "cheryl"]);
+    });
+    it("compares numbers naturally (not lexicographically)", () => {
+      const actual = [9, 10, 8].sort(Cmp.natural);
+      expect(actual).toEqual([8, 9, 10]);
+    });
+  });
+
+  describe("rev", () => {
+    it("defines a reverse-natural order by default", () => {
+      const actual = [9, 10, 8].sort(Cmp.rev());
+      expect(actual).toEqual([10, 9, 8]);
+    });
+    it("reverses the order of a given sub-comparator", () => {
+      const byBidDesc = Cmp.rev(Cmp.comparing((x) => x.bid));
+      const actual = [{ bid: 9 }, { bid: 10 }, { bid: 8 }].sort(byBidDesc);
+      expect(actual).toEqual([{ bid: 10 }, { bid: 9 }, { bid: 8 }]);
+    });
+  });
+
+  describe("comparing", () => {
+    it("accepts a simple accessor as only argument", () => {
+      const byLength = Cmp.comparing((s) => s.length);
+      const actual = ["alice", "bob", "cheryl"].sort(byLength);
+      expect(actual).toEqual(["bob", "alice", "cheryl"]);
+    });
+    it("keeps ties stable", () => {
+      const byLength = Cmp.comparing((s) => s.length);
+      const actual = ["alice", "bob", "bab", "beb"].sort(byLength);
+      expect(actual).toEqual(["bob", "bab", "beb", "alice"]);
+    });
+    it("accepts an accessor and sub-comparator", () => {
+      const byRevLength = Cmp.comparing((s) => s.length, Cmp.rev());
+      const actual = ["alice", "bob", "cheryl"].sort(byRevLength);
+      expect(actual).toEqual(["cheryl", "alice", "bob"]);
+    });
+  });
+
+  describe("first", () => {
+    it("uses the first nonzero comparator result", () => {
+      const shortlex = Cmp.first([Cmp.comparing((s) => s.length), Cmp.natural]);
+      const actual = ["bob", "bab", "beb", "jo", "alice"].sort(shortlex);
+      expect(actual).toEqual(["jo", "bab", "beb", "bob", "alice"]);
+    });
+    it("returns zero when all comparators return zero", () => {
+      const byHookOrByCrook = Cmp.first([
+        Cmp.comparing((x) => x.hook),
+        Cmp.comparing((x) => x.crook),
+      ]);
+      const x = { hook: 1, crook: 2, brook: 3 };
+      const y = { hook: 1, crook: 2, brook: 5 };
+      expect(byHookOrByCrook(x, y)).toEqual(0);
+    });
+  });
+
+  describe("nullsLast", () => {
+    it("pushes nulls to the end of natural order by default", () => {
+      const actual = [9, null, 10, 8].sort(Cmp.nullsLast());
+      expect(actual).toEqual([8, 9, 10, null]);
+    });
+    it("accepts a sub-comparator only invoked on non-nulls", () => {
+      const byLength = Cmp.comparing((s) => s.length);
+      const actual = ["alice", null, "bob"].sort(Cmp.nullsLast(byLength));
+      expect(actual).toEqual(["bob", "alice", null]);
+    });
+  });
+});
