5ac354d0a2e004aa9f053748f525fae4b5ca7856
Dandelion Mané <decentralion@dandelion.io>
2022-03-17 14:57:11 -0700

    backfill: move AB-specific concepts to new table

--- src/db/backfills/index.js
+++ src/db/backfills/index.js
@@ -6,6 +6,7 @@ const backfillModules = [
   "./populateOpenseaIngestionQueue",
   "./openseaInvalidateAsksWithSubsequentSale",
   "./openseaSkipPrivateAsks",
+  "./abConceptMigrations",
   // ...
 ];
 
