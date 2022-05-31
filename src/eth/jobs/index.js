3ec4724ee3a3db57d6adbdb44505bb314de0f865
William Chargin <wchargin@gmail.com>
2022-05-31 13:00:25 -0400

    eth: document job interface

--- src/eth/jobs/index.js
+++ src/eth/jobs/index.js
@@ -1,5 +1,37 @@
 const makeEchoJob = require("./echo");
 
+/*::
+interface Job {
+  /// A cosmetic name for the job, like "echo" or "erc721Transfers(0xa7d8d9...)".
+  name(): string;
+
+  /// The block number at which ingestion should start. A value of 0 indicates
+  /// that the whole chain should be processed. A positive value indicates that
+  /// it is known that there are no events until that block, so some history
+  /// can be skipped. (E.g., if tracking transfers on a contract, this might be
+  /// the block in which that contract was deployed.)
+  startBlock(): number;
+
+  /// The maximum number of blocks that can be processed (with `up`) in a
+  /// reasonable amount of time. A typical value is 2000, since that's
+  /// Alchemy's cap on the `getFilterLogs` RPC range.
+  blockBatchSize(): number;
+
+  /// Processes a range of blocks. This will be called inside a database
+  /// transaction; the method MUST NOT commit or roll back the transaction, but
+  /// instead either resolve or reject the promise. It MUST be possible to
+  /// revert this by calling `down(...)` for each block in the given range, so
+  /// data added by this method should generally be keyed by block hash.
+  async up({ client, provider, minBlock: number, maxBlock: number }): Promise<void>;
+
+  /// Processes a range of blocks. This will be called inside a database
+  /// transaction; the method MUST NOT commit or roll back the transaction. The
+  /// returned promise may revert if in a broken state, but this will leave the
+  /// system at a standstill and so SHOULD be avoided.
+  async down({ client, blockHash, blockNumber }): Promise<void>;
+}
+*/
+
 const JOBS = [
   makeEchoJob(),
   // ...
