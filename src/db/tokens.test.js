fc483bab1036988f390c302a8b4f93cd4a20baa4
William Chargin <wchargin@gmail.com>
2022-06-02 13:42:21 -0400

    db: remove dead (never-used) test helpers

--- src/db/tokens.test.js
+++ src/db/tokens.test.js
@@ -42,69 +42,6 @@ describe("db/tokens", () => {
     return result;
   }
 
-  async function getProject({ client, projectId }) {
-    const res = await client.query(
-      `
-      SELECT
-        project_id AS "projectId",
-        name as "name",
-        max_invocations AS "maxInvocations",
-        artist_name AS "artistName",
-        description AS "description",
-        aspect_ratio AS "aspectRatio",
-        num_tokens AS "numTokens",
-        slug AS "slug",
-        token_contract AS "tokenContract",
-        image_template AS "imageTemplate"
-      FROM projects
-      WHERE project_id = $1
-      `,
-      [projectId]
-    );
-    if (res.rows.length === 0) return null;
-    const row = res.rows[0];
-    row.tokenContract = bufToAddress(row.tokenContract);
-    return row;
-  }
-
-  async function addTestData(client) {
-    const scriptJson = JSON.stringify({ aspectRatio: "1" });
-    const projects = [
-      { projectId: 1, name: "A", maxInvocations: 5, scriptJson },
-      { projectId: 2, name: "B", maxInvocations: 5, scriptJson },
-    ];
-    const s = JSON.stringify;
-    const tokens = [
-      { tokenId: 1000000, rawTokenData: s({ features: { Size: "small" } }) },
-      { tokenId: 1000001, rawTokenData: s({ features: { Size: "large" } }) },
-      {
-        tokenId: 2000000,
-        rawTokenData: s({ features: { Size: "small", Color: "red" } }),
-      },
-      {
-        tokenId: 2000001,
-        rawTokenData: s({ features: { Size: "large", Color: "green" } }),
-      },
-      {
-        tokenId: 2000002,
-        rawTokenData: s({ features: { Size: "small", Color: "blue" } }),
-      },
-    ];
-    const projectIds = await Promise.all(
-      projects.map((p) => artblocks.addProject({ client, project: p }))
-    );
-    await Promise.all(
-      tokens.map((t) =>
-        artblocks.addToken({
-          client,
-          artblocksTokenId: t.tokenId,
-          rawTokenData: t.rawTokenData,
-        })
-      )
-    );
-    return projectIds;
-  }
-
   it(
     "supports tokenSummariesByOnChainId",
     withTestDb(async ({ client }) => {
