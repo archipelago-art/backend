c1684e770c0edf9f1616f093275b2080239a1181
William Chargin <wchargin@gmail.com>
2022-06-06 12:25:12 -0400

    signal: add optional timeout to `waitAndReset`

--- src/util/signal.js
+++ src/util/signal.js
@@ -13,9 +13,28 @@ class Signal {
   async wait() {
     return await this._adHocPromise.promise;
   }
-  async waitAndReset() {
-    const res = await this.wait();
+  /**
+   * Wait for the signal to be set and then resets it, or rejects without
+   * resetting if `timeout` ms elapse (if `timeout` is non-null).
+   *
+   * This differs from `Promise.race([sleepMs(t), s.wait().then(s.reset)])` in
+   * that it only resets the signal if it resolves *during* this call.
+   */
+  async waitAndReset(timeout) {
+    let task, timer;
+    if (timeout == null) {
+      task = this.wait();
+    } else {
+      task = Promise.race([
+        new Promise((res, rej) => {
+          timer = setTimeout(() => rej("timed out"), timeout);
+        }),
+        this.wait(),
+      ]);
+    }
+    const res = await task;
     this.reset();
+    if (timer != null) clearTimeout(timer);
     return res;
   }
 }
