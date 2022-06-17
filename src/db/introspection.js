ba2153334861ce479c6ad71cfde73167adc0b82e
William Chargin <wchargin@gmail.com>
2022-06-17 15:28:33 -0400

    introspection: add `columnExists`
    Facilitates column-renaming migrations.

--- src/db/introspection.js
+++ src/db/introspection.js
@@ -9,6 +9,18 @@ async function tableExists({ client, schema = "public", table }) {
   return res.rowCount > 0;
 }
 
+async function columnExists({ client, schema = "public", table, column }) {
+  const res = await client.query(
+    `
+    SELECT 1 FROM information_schema.columns
+    WHERE table_schema = $1 AND table_name = $2 AND column_name = $3
+    `,
+    [schema, table, column]
+  );
+  return res.rowCount > 0;
+}
+
 module.exports = {
   tableExists,
+  columnExists,
 };
--- src/db/introspection.test.js
+++ src/db/introspection.test.js
@@ -1,5 +1,5 @@
 const { testDbProvider } = require("./testUtil");
-const { tableExists } = require("./introspection");
+const { columnExists, tableExists } = require("./introspection");
 
 describe("db/introspection", () => {
   const withTestDb = testDbProvider({ migrate: false });
@@ -50,6 +50,34 @@ describe("db/introspection", () => {
         expect(await tableExists({ client, table })).toBe(false);
         const schema = "information_schema";
         expect(await tableExists({ client, table, schema })).toBe(true);
+      })
+    );
+  });
+
+  describe("columnExists", () => {
+    it(
+      "properly detects existence of columns",
+      withTestDb(async ({ client }) => {
+        await client.query(
+          "BEGIN; CREATE TABLE t1(x int, y int); CREATE TABLE t2(x int);"
+        );
+        const table = "t2";
+        const column = "y";
+        expect(await columnExists({ client, table, column })).toBe(false);
+        await client.query("ALTER TABLE t2 ADD COLUMN y int");
+        expect(await columnExists({ client, table, column })).toBe(true);
+        await client.query("ROLLBACK");
+      })
+    );
+
+    it(
+      "only looks in the given schema",
+      withTestDb(async ({ client }) => {
+        const table = "columns";
+        const column = "column_name";
+        expect(await tableExists({ client, table, column })).toBe(false);
+        const schema = "information_schema";
+        expect(await tableExists({ client, table, column, schema })).toBe(true);
       })
     );
   });
