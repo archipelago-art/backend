archipelago-art/backend

commit 632dd5ac580d506dc9e7adf6b7d49fcb65cfab67

use api contract info for img ingestion

src/img/ingest2.js
@@ -7,9 +7,20 @@ const fetch = require("node-fetch");
 
 const log = require("../util/log")(__filename);
 
-const { imageInfo } = require("./contracts");
 const { ORIG, targets } = require("./ingestTargets");
 const downloadAtomic = require("../util/gcsDownloadAtomic");
+
+const { contractForAddress } = require("../api/contracts");
+
+function imageInfo(token) {
+  const res = contractForAddress(token.tokenContract);
+  if (res == null) {
+    throw new Error(
+      `no info for token ${token.slug} #${token.tokenIndex} (missing contract ${token.tokenContract})`
+    );
+  }
+  return res;
+}
 
 const LOOP_TIMEOUT_MS = 15 * 1000;
 
@@ -62,7 +73,7 @@ function imagePath(token, targetName, options) {
     slash: false,
     ...options,
   };
-  const { projectName } = imageInfo(token);
+  const { name } = imageInfo(token);
   const { tokenContract, onChainTokenId } = token;
   const idHigh = Math.floor(onChainTokenId / 1e6).toFixed(0);
   const idLow = Math.floor(onChainTokenId % 1e6)

src/img/contracts.js
@@ -1,47 +1,0 @@
-const {
-  CONTRACT_ARTBLOCKS_LEGACY,
-  CONTRACT_ARTBLOCKS_STANDARD,
-} = require("../db/artblocks");
-const AUTOGLYPHS_CONTRACT = require("../db/autoglyphs").CONTRACT_ADDRESS;
-const { CRYPTOADZ_CONTRACT } = require("../db/cryptoadz");
-
-const CONTRACT_IMAGE_INFO = {
-  [CONTRACT_ARTBLOCKS_LEGACY]: {
-    projectName: "artblocks",
-    externalUrl: "https://media.artblocks.io/{tokenid}.png",
-  },
-  [CONTRACT_ARTBLOCKS_STANDARD]: {
-    projectName: "artblocks",
-    externalUrl: "https://media.artblocks.io/{tokenid}.png",
-  },
-  [AUTOGLYPHS_CONTRACT]: {
-    projectName: "autoglyphs",
-    externalUrl:
-      "https://larvalabs.com/public/images/autoglyphs/glyph{tokenid}.svg",
-  },
-  [CRYPTOADZ_CONTRACT]: {
-    projectName: "cryptoadz",
-    externalUrl:
-      "https://qiydg7uxbfyhvl4jfhmlsqrzcjirat6w7xpyjiogwuifswrfm4.arweave.net/gjAzfpcJcHqviSnYuUI5ElEQT9b934ShxrUQ-WVolZ0/{tokenid}.png",
-  },
-};
-
-function imageInfo(token) {
-  const res = CONTRACT_IMAGE_INFO[token.tokenContract];
-  if (res == null) {
-    throw new Error(
-      `no info for token ${token.slug} #${token.tokenIndex} (missing contract ${token.tokenContract})`
-    );
-  }
-  return res;
-}
-
-function nameForContract(contract) {
-  const res = CONTRACT_IMAGE_INFO[contract];
-  if (res == null) {
-    throw new Error(`no info for contract ${contract}`);
-  }
-  return res.projectName;
-}
-
-module.exports = { imageInfo, nameForContract };

commit 3c9c35653e27897cebc2af17a1840dcaa4d3157a

ingest2: fix image path construction
Job broken due to `ReferenceError`.

Fixes: 632dd5ac580d ("use api contract info for img ingestion")

src/img/ingest2.js
@@ -73,7 +73,7 @@ function imagePath(token, targetName, options) {
     slash: false,
     ...options,
   };
-  const { name } = imageInfo(token);
+  const { name: projectName } = imageInfo(token);
   const { tokenContract, onChainTokenId } = token;
   const idHigh = Math.floor(onChainTokenId / 1e6).toFixed(0);
   const idLow = Math.floor(onChainTokenId % 1e6)