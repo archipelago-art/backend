c2de53ede275b6f47e50e13b7531c28f3780aa9a
Ben Weisel <bweisel@gmail.com>
2022-08-22 14:41:57 -0600

    Use retryFetch

--- src/artacle/artacle.js
+++ src/artacle/artacle.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+const { fetchWithRetries } = require("../scrape/retryFetch");
 
 const apiKey = process.env.ARTACLE_API_KEY;
 const artacleApiBase = "https://api.artacle.io/v1";
@@ -9,14 +9,16 @@ async function getCollections() {
 }
 
 async function getCollectionRarity(artblocksCollectionIndex) {
-  return await _paginated_fetch(`${artacleApiBase}/collections/${artblocksCollectionIndex}/rarity`);
+  return await _paginated_fetch(
+    `${artacleApiBase}/collections/${artblocksCollectionIndex}/rarity`
+  );
 }
 
 async function getTokenRarity(artblocksCollectionId, artblocksTokenId) {
-  const response = await fetch(
+  const response = await fetchWithRetries(
     `${artacleApiBase}/collections/${artblocksCollectionId}/token/${artblocksTokenId}/rarity`
   );
-  return await response.json().data;
+  return await JSON.parse(response.text).data;
 }
 
 async function _paginated_fetch(url) {
@@ -25,8 +27,10 @@ async function _paginated_fetch(url) {
   let lastLength = 0;
 
   do {
-    const responseRaw = await fetch(`${url}?apiKey=${apiKey}&offset=${offset}&limit=${artacleLimit}`);
-    const responseJson = await responseRaw.json();
+    const responseRaw = await fetchWithRetries(
+      `${url}?apiKey=${apiKey}&offset=${offset}&limit=${artacleLimit}`
+    );
+    const responseJson = await JSON.parse(responseRaw.text);
     results = results.concat(responseJson.data);
     lastLength = responseJson.data.length;
     offset += artacleLimit;
