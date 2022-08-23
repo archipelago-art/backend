5790d466406e32d05da5cebdccc913edb1747432
William Chargin <wchargin@gmail.com>
2022-08-23 12:37:33 -0700

    db: add `token_rarity` table

---
+++ src/db/migrations/0132_token_rarity.js
@@ -0,0 +1,14 @@
+async function up({ client }) {
+  await client.query(`
+    CREATE TABLE token_rarity (
+      token_id tokenid PRIMARY KEY REFERENCES tokens(token_id),
+      project_id projectid NOT NULL REFERENCES projects(project_id),
+      rarity_rank integer,
+      update_time timestamptz NOT NULL
+    );
+    CREATE INDEX token_rarity_project_id_rarity_rank
+      ON token_rarity (project_id, rarity_rank);
+  `);
+}
+
+module.exports = { up };
