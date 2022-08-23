58ba3a6222688edba2f8cb7d59f2bf5957a16648
William Chargin <wchargin@gmail.com>
2022-08-23 11:40:54 -0700

    artacle: fix whitespace

--- src/db/artacle.js
+++ src/db/artacle.js
@@ -1,14 +1,14 @@
 async function getRarityForProjectTokens({ client, projectId }) {
   const res = await client.query(
     `
-      SELECT tr.token_id AS "tokenId",
-          t.token_index AS "tokenIndex",
-          tr.rarity_rank AS "rarityRank"
-      FROM token_rarity tr
-      JOIN tokens t USING (token_id)
-      WHERE tr.project_id = $1::projectid
-      ORDER BY tr.rarity_rank
-      `,
+    SELECT tr.token_id AS "tokenId",
+        t.token_index AS "tokenIndex",
+        tr.rarity_rank AS "rarityRank"
+    FROM token_rarity tr
+    JOIN tokens t USING (token_id)
+    WHERE tr.project_id = $1::projectid
+    ORDER BY tr.rarity_rank
+    `,
     [projectId]
   );
   return res.rows;
@@ -21,22 +21,22 @@ async function updateTokenRarity({
 }) {
   await client.query(
     `
-        WITH updates AS (
-          SELECT
-            inputs.token_id,
-            tokens.project_id,
-            inputs.rarity_rank
-          FROM unnest($1::tokenid[], $2::int[]) as inputs(token_id, rarity_rank)
-          JOIN tokens USING (token_id)
-        )
-        INSERT INTO token_rarity (token_id, project_id, rarity_rank, update_time)
-        SELECT token_id, project_id, rarity_rank, now()
-        FROM updates
-        ON CONFLICT (token_id) DO UPDATE 
-          SET rarity_rank = excluded.rarity_rank, 
-          project_id = excluded.project_id, 
-          update_time = excluded.update_time;
-      `,
+    WITH updates AS (
+      SELECT
+        inputs.token_id,
+        tokens.project_id,
+        inputs.rarity_rank
+      FROM unnest($1::tokenid[], $2::int[]) as inputs(token_id, rarity_rank)
+      JOIN tokens USING (token_id)
+    )
+    INSERT INTO token_rarity (token_id, project_id, rarity_rank, update_time)
+    SELECT token_id, project_id, rarity_rank, now()
+    FROM updates
+    ON CONFLICT (token_id) DO UPDATE
+      SET rarity_rank = excluded.rarity_rank,
+      project_id = excluded.project_id,
+      update_time = excluded.update_time;
+    `,
     [updates.map((x) => x.tokenId), updates.map((x) => x.rarityRank)]
   );
 }
@@ -44,13 +44,13 @@ async function updateTokenRarity({
 async function getTokenRarity({ client, tokenId }) {
   const res = await client.query(
     `
-      SELECT 
-        tr.rarity_rank AS "rarityRank",
-        (SELECT num_tokens FROM projects WHERE project_id = t.project_id) AS "total"
-      FROM token_rarity tr
-      JOIN tokens t USING (token_id)
-      WHERE token_id = $1::tokenid
-      `,
+    SELECT
+      tr.rarity_rank AS "rarityRank",
+      (SELECT num_tokens FROM projects WHERE project_id = t.project_id) AS "total"
+    FROM token_rarity tr
+    JOIN tokens t USING (token_id)
+    WHERE token_id = $1::tokenid
+    `,
     [tokenId]
   );
   return res.rows[0];
